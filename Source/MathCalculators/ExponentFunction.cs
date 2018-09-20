b251cf69b6f7cf52d94e7df70f21c59118bd1e59
Justin Welsch <j.welsch@gmail.com>
2018-09-20 19:31:54 -0400

    Add exponent function

---
+++ Source/MathCalculators/ExponentFunction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    [Function]
+    public class ExponentFunction : IFunction
+    {
+        public IFunctionInfo FunctionInfo
+        {
+            get;
+            private set;
+        }
+
+        public ExponentFunction()
+        {
+            this.FunctionInfo = new DefaultFunctionInfo(new Version("1.0.0"), "Exponent", "Raise a number to an exponent.", "exponent");
+        }
+
+        public IValue Calculate(PhaseHandler phaseHandler)
+        {
+            if (phaseHandler == null)
+            {
+                throw new ArgumentNullException(nameof(phaseHandler));
+            }
+
+            var phase = new DefaultPhase(
+                "Specify Operands",
+                "Specify exponential equation.",
+                new AgnosticValue(0.0, new ValueInfo("Base")),
+                new AgnosticValue(0.0, new ValueInfo("Exponent")));
+
+            var phaseValues = phaseHandler(phase);
+
+            var values = phaseValues.ToArray();
+
+            var result = Math.Pow(TypeConverter.ToObject<double>(values[0].Value), TypeConverter.ToObject<double>(values[1].Value));
+
+            return new AgnosticValue(result);
+        }
+    }
+}
---
+++ Tests/MathCaculators.Functional/ExponentFunctionConstructorShould.cs
@@ -0,0 +1,20 @@
+﻿using MathCalculators;
+using Xunit;
+
+namespace MathCaculators.Functional
+{
+    public class ExponentFunctionConstructorShould
+    {
+        [Fact]
+        public void SuccessfullySetFunctionInfo()
+        {
+            var function = new ExponentFunction();
+
+            Assert.NotNull(function.FunctionInfo);
+            Assert.Equal("Exponent", function.FunctionInfo.Name);
+            Assert.NotEqual(string.Empty, function.FunctionInfo.Description);
+            Assert.NotEmpty(function.FunctionInfo.Tags);
+            Assert.NotNull(function.FunctionInfo.Version);
+        }
+    }
+}
