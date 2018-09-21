172e23db336fb0b81bde67305a0144d0ea00a87e
Justin Welsch <j.welsch@gmail.com>
2018-09-20 21:41:47 -0400

    Add sine function and tests

---
+++ Source/MathCalculators/SineFunction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    [Function]
+    public class SineFunction : BaseFunction
+    {
+        public SineFunction()
+            : base(new DefaultFunctionInfo(new Version("1.0.0"), "Sine", "Find the sine of an angle.", "sine", "sin"))
+        {
+        }
+
+        public override IValue Calculate(PhaseHandler phaseHandler)
+        {
+            var phase = new DefaultPhase(
+                "Specify Operands",
+                "Specify angle to find the sine of.",
+                new AgnosticValue(0.0, new ValueInfo("Angle")));
+
+            var values = DoPhase(phaseHandler, phase);
+
+            var result = Math.Sin(TypeConverter.ToObject<double>(values[0].Value));
+
+            return new AgnosticValue(result);
+        }
+    }
+}
--- Source/XCalculateLib/BaseFunction.cs
+++ Source/XCalculateLib/BaseFunction.cs
@@ -1,4 +1,7 @@
-﻿namespace XCalculateLib
+﻿using System;
+using System.Linq;
+
+namespace XCalculateLib
 {
     public abstract class BaseFunction : IFunction
     {
@@ -14,5 +17,17 @@
         }
 
         public abstract IValue Calculate(PhaseHandler phaseHandler);
+
+        protected static IValue[] DoPhase(PhaseHandler phaseHandler, IPhase phase)
+        {
+            if (phaseHandler == null)
+            {
+                throw new ArgumentNullException(nameof(phaseHandler));
+            }
+
+            var phaseValues = phaseHandler(phase);
+
+            return phaseValues.ToArray();
+        }
     }
 }
