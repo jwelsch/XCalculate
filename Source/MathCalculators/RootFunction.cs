336cf1f09cb00801ad5286422d8a81f3c02a2b24
Justin Welsch <j.welsch@gmail.com>
2018-09-20 21:27:25 -0400

    Add root function and tests

---
+++ Source/MathCalculators/RootFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    [Function]
+    public class RootFunction : BaseFunction
+    {
+        public RootFunction()
+            : base(new DefaultFunctionInfo(new Version("1.0.0"), "Root", "Find the root of a number.", "root"))
+        {
+        }
+
+        public override IValue Calculate(PhaseHandler phaseHandler)
+        {
+            if (phaseHandler == null)
+            {
+                throw new ArgumentNullException(nameof(phaseHandler));
+            }
+
+            var phase = new DefaultPhase(
+                "Specify Operands",
+                "Specify root operation values.",
+                new AgnosticValue(0.0, new ValueInfo("Radicand", "Radicand of the root.")),
+                new AgnosticValue(2.0, new ValueInfo("Index", "Index of the root.")));
+
+            var phaseValues = phaseHandler(phase);
+
+            var values = phaseValues.ToArray();
+
+            var result = Math.Pow(TypeConverter.ToObject<double>(values[0].Value), 1.0 / TypeConverter.ToObject<double>(values[1].Value));
+
+            return new AgnosticValue(result);
+        }
+    }
+}
---
+++ Tests/MathCaculators.Functional/RootFunctionConstructorShould.cs
@@ -0,0 +1,20 @@
+﻿using MathCalculators;
+using Xunit;
+
+namespace MathCaculators.Functional
+{
+    public class RootFunctionConstructorShould
+    {
+        [Fact]
+        public void SuccessfullySetFunctionInfo()
+        {
+            var function = new RootFunction();
+
+            Assert.NotNull(function.FunctionInfo);
+            Assert.Equal("Root", function.FunctionInfo.Name);
+            Assert.NotEqual(string.Empty, function.FunctionInfo.Description);
+            Assert.NotEmpty(function.FunctionInfo.Tags);
+            Assert.NotNull(function.FunctionInfo.Version);
+        }
+    }
+}
