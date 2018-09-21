fbb1ffd555c632d00fe9610b14885a8572a57106
Justin Welsch <j.welsch@gmail.com>
2018-09-21 08:31:24 -0400

    Add Cosine and Tangent functions and tests

---
+++ Source/MathCalculators/TangentFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    public class TangentFunction : BaseFunction
+    {
+        public TangentFunction()
+            : base(new DefaultFunctionInfo(new Version("1.0.0"), "Tangent", "Find the tangent of an angle.", "tangent", "tan"))
+        {
+        }
+
+        public override IValue Calculate(PhaseHandler phaseHandler)
+        {
+            var phase = new DefaultPhase(
+                "Specify Operands",
+                "Specify angle to find the tangent of.",
+                new AgnosticValue(0.0, new ValueInfo("Angle")));
+
+            var values = DoPhase(phaseHandler, phase);
+
+            var result = Math.Tan(TypeConverter.ToObject<double>(values[0].Value));
+
+            return new AgnosticValue(result);
+        }
+    }
+}
---
+++ Tests/MathCaculators.Functional/CosineFunctionCalculateShould.cs
@@ -0,0 +1,56 @@
+﻿using MathCalculators;
+using System;
+using XCalculateLib;
+using Xunit;
+
+namespace MathCaculators.Functional
+{
+    public class CosineFunctionCalculateShould
+    {
+        [Fact]
+        public void SuccessfullyCalculateCosineOfPositiveAngle()
+        {
+            var function = new CosineFunction();
+
+            var result = function.Calculate(p =>
+            {
+                p.Inputs[0].Value = 60;
+
+                return p.Inputs;
+            });
+
+            Assert.Equal(typeof(double), result.ValueType);
+            Assert.Equal(Math.Cos(60), TypeConverter.ToObject<double>(result.Value));
+        }
+
+        [Fact]
+        public void SuccessfullyCalculateCosineOfNegativeAngle()
+        {
+            var function = new CosineFunction();
+
+            var result = function.Calculate(p =>
+            {
+                p.Inputs[0].Value = -54;
+
+                return p.Inputs;
+            });
+
+            Assert.Equal(typeof(double), result.ValueType);
+            Assert.Equal(Math.Cos(-54), TypeConverter.ToObject<double>(result.Value));
+        }
+
+        [Fact]
+        public void SuccessfullyCalculateCosineWithNoAngleSpecified()
+        {
+            var function = new CosineFunction();
+
+            var result = function.Calculate(p =>
+            {
+                return p.Inputs;
+            });
+
+            Assert.Equal(typeof(double), result.ValueType);
+            Assert.Equal(Math.Cos(0.0), TypeConverter.ToObject<double>(result.Value));
+        }
+    }
+}
