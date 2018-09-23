72f8ed772f0f7a3b0beace46123ee5bb1dc5742e
Justin Welsch <j.welsch@gmail.com>
2018-09-23 13:53:24 -0400

    Add simple CalculatorController for routing test

---
+++ Source/XCalculate.Web.App/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XCalculate.Web.App.Models;
+using XCalculate.Web.Core.Interfaces;
+
+namespace XCalculate.Web.App.Controllers
+{
+    [Route("[controller]")]
+    public class CalculatorController : Controller
+    {
+        private readonly ICalculatorService calculatorService;
+
+        public CalculatorController(ICalculatorService calculatorService)
+        {
+            this.calculatorService = calculatorService;
+        }
+
+        [Route("{id}")]
+        public IActionResult Index(int id)
+        {
+            var calculator = this.calculatorService.GetById(id);
+
+            var vm = new CalculatorIndexViewModel()
+            {
+                Id = calculator.Id,
+                Name = calculator.Module.Function.FunctionInfo.Name
+            };
+
+            return View(vm);
+        }
+    }
+}
