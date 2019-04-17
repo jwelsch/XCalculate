91996c745980cb6991fa12a9def7f34b1d0ff496
Justin Welsch <j.welsch@gmail.com>
2019-04-17 17:39:13 -0400

    Add physics calculator DLL and force equation

---
+++ Source/PhysicsCalculators/ForceFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using XCalculateLib;
+
+namespace PhysicsCalculators
+{
+    [Function]
+    public class ForceFunction : BaseFunction
+    {
+        public ForceFunction()
+            : base(
+                  new FunctionInfo(new Version("1.0.0"), "Force Equation", new ValueInfo("Force", "Force of an object"), "Calculates the force of an object.", "physics", "force"),
+                  new AgnosticValue(0.0, new ValueInfo("Mass", "Mass of the object.")),
+                  new AgnosticValue(0.0, new ValueInfo("Acceleration", "Acceleration of the object.")))
+        {
+        }
+
+        public override IValue[] Calculate(IValue[] inputs)
+        {
+            this.CheckInputs(inputs);
+
+            var mass = GetValue<double>(inputs[0]);
+            var acceleration = GetValue<double>(inputs[1]);
+
+            //
+            // F = ma
+            //
+            var result = mass * acceleration;
+
+            return this.CreateResults(result);
+        }
+    }
+}
