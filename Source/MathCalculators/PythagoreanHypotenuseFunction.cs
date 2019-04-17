88965370df98abd7830ff01fa5c8c6b03cf35e08
Justin Welsch <j.welsch@gmail.com>
2019-04-17 17:22:44 -0400

    Add Pythagorean equations

---
+++ Source/MathCalculators/PythagoreanHypotenuseFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    [Function]
+    public class PythagoreanHypotenuseFunction : BaseFunction
+    {
+        public PythagoreanHypotenuseFunction()
+            : base(
+                  new FunctionInfo(new Version("1.0.0"), "Pythagorean Equation (Hypotenuse)", new ValueInfo("Hypotenuse", "Length of the hypotenuse"), "Calculates the length of the hypotenuse of a right triangle.", "geometry", "pythagorean", "hypotenuse"),
+                  new AgnosticValue(0.0, new ValueInfo("Side A", "Length of side A.")),
+                  new AgnosticValue(0.0, new ValueInfo("Side B", "Length of side B.")))
+        {
+        }
+
+        public override IValue[] Calculate(IValue[] inputs)
+        {
+            this.CheckInputs(inputs);
+
+            var sideA = GetValue<double>(inputs[0]);
+            var sideB = GetValue<double>(inputs[1]);
+
+            var result = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+
+            return this.CreateResults(result);
+        }
+    }
+}
---
+++ Source/MathCalculators/PythagoreanSideFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XCalculateLib;
+
+namespace MathCalculators
+{
+    [Function]
+    public class PythagoreanSideFunction : BaseFunction
+    {
+        public PythagoreanSideFunction()
+            : base(
+                  new FunctionInfo(new Version("1.0.0"), "Pythagorean Equation (Side)", new ValueInfo("Side", "Length of a side"), "Calculates the length of a side (not the hypotenuse) of a right triangle.", "geometry", "pythagorean"),
+                  new AgnosticValue(0.0, new ValueInfo("Hypotenuse", "Length of the hypotenuse.")),
+                  new AgnosticValue(0.0, new ValueInfo("Side", "Length of a side.")))
+        {
+        }
+
+        public override IValue[] Calculate(IValue[] inputs)
+        {
+            this.CheckInputs(inputs);
+
+            var hypotenuse = GetValue<double>(inputs[0]);
+            var side = GetValue<double>(inputs[1]);
+
+            //
+            // h^2 - a^2 = b^2
+            //
+            var result = Math.Sqrt(Math.Pow(hypotenuse, 2) - Math.Pow(side, 2));
+
+            return this.CreateResults(result);
+        }
+    }
+}
